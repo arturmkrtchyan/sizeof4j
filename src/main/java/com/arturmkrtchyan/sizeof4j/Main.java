2b85c5c2fc66840a20958539a740c65d1798d087
Artur Mkrtchyan <mkrtchyan.artur@gmail.com>
2014-10-17 16:46:06 +0200

    Added sample to README

--- src/main/java/com/arturmkrtchyan/sizeof4j/Main.java
+++ src/main/java/com/arturmkrtchyan/sizeof4j/Main.java
@@ -1,31 +1,45 @@
 package com.arturmkrtchyan.sizeof4j;
 
-
-import com.arturmkrtchyan.sizeof4j.test.A;
-import com.arturmkrtchyan.sizeof4j.test.B;
-import com.arturmkrtchyan.sizeof4j.test.C;
-
 public class Main {
 
     public static void main(String[] args) {
 
         JvmUtil.printAll();
 
-        System.out.println(SizeOf.byteSize());
-        System.out.println(SizeOf.booleanSize());
-        System.out.println(SizeOf.shortSize());
-        System.out.println(SizeOf.charSize());
-        System.out.println(SizeOf.intSize());
-        System.out.println(SizeOf.floatSize());
-        System.out.println(SizeOf.longSize());
-        System.out.println(SizeOf.doubleSize());
+        System.out.println("\n\nSizeOf");
+        System.out.println("=================================");
+        System.out.println("byte:       " + SizeOf.byteSize());
+        System.out.println("boolean:    " + SizeOf.booleanSize());
+        System.out.println("short:      " + SizeOf.shortSize());
+        System.out.println("char:       " + SizeOf.charSize());
+        System.out.println("int:        " + SizeOf.intSize());
+        System.out.println("float:      " + SizeOf.floatSize());
+        System.out.println("long:       " + SizeOf.longSize());
+        System.out.println("double:     " + SizeOf.doubleSize());
 
-        System.out.println(SizeOf.objectShallowSize(new Object()));
-        System.out.println(SizeOf.objectShallowSize(new Integer(2)));
+        System.out.println("--------------------------");
+        System.out.println("Object:     " + SizeOf.objectShallowSize(new Object()));
+        System.out.println("Integer:    " + SizeOf.objectShallowSize(new Integer(2)));
+        System.out.println("String:     " + SizeOf.objectShallowSize(new String("aaa")));
 
-        System.out.println(SizeOf.objectShallowSize(new A()));
-        System.out.println(SizeOf.objectShallowSize(new B()));
-        System.out.println(SizeOf.objectShallowSize(new C()));
+        System.out.println("--------------------------");
+        System.out.println("A:          " + SizeOf.objectShallowSize(new A()));
+        System.out.println("B:          " + SizeOf.objectShallowSize(new B()));
+        System.out.println("C:          " + SizeOf.objectShallowSize(new C()));
+    }
+
+    private static class A {
+        private int i;
+    }
+
+    private static class B {
+        private int i;
+        private java.util.Locale l = java.util.Locale.US;
+    }
+
+    private static class C {
+        private int i;
+        private java.util.concurrent.ConcurrentHashMap chm = new java.util.concurrent.ConcurrentHashMap();
     }
 
 }
