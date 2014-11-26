a0a098acf5bd053f2a82528819f4e9ecb27fe0da
Artur Mkrtchyan <mkrtchyan.artur@gmail.com>
2014-11-26 10:46:08 +0100

    Added more tests

--- src/test/java/com/arturmkrtchyan/sizeof4j/JvmUtilsTest.java
+++ src/test/java/com/arturmkrtchyan/sizeof4j/JvmUtilsTest.java
@@ -23,4 +23,63 @@ public class JvmUtilsTest {
     public void unsafe() {
         assertNotNull(JvmUtil.unsafe());
     }
+
+    @Test
+    public void committedMemory() {
+        final long memory = JvmUtil.committedMemory();
+        assertTrue(memory > 0);
+    }
+
+    @Test
+    public void initialMemory() {
+        final long memory = JvmUtil.initialMemory();
+        assertTrue(memory > 0);
+    }
+
+    @Test
+    public void maxMemory() {
+        final long memory = JvmUtil.maxMemory();
+        assertTrue(memory > 0);
+    }
+
+    @Test
+    public void usedMemory() {
+        final long memory = JvmUtil.usedMemory();
+        assertTrue(memory > 0);
+    }
+
+    @Test
+    public void jreSpecVersion() {
+        assertNotNull(JvmUtil.jreSpecVersion());
+        assertNotEquals(JvmUtil.jreSpecVersion(), "");
+    }
+
+    @Test
+    public void jreVersion() {
+        assertNotNull(JvmUtil.jreVersion());
+        assertNotEquals(JvmUtil.jreVersion(), "");
+    }
+
+    @Test
+    public void osArch() {
+        assertNotNull(JvmUtil.osArch());
+        assertNotEquals(JvmUtil.osArch(), "");
+    }
+
+    @Test
+    public void vmArch() {
+        assertNotNull(JvmUtil.vmArch());
+        assertNotEquals(JvmUtil.vmArch(), "");
+    }
+
+    @Test
+    public void vmSpecVersion() {
+        assertNotNull(JvmUtil.vmSpecVersion());
+        assertNotEquals(JvmUtil.vmSpecVersion(), "");
+    }
+
+    @Test
+    public void memoryLayout() {
+        assertNotNull(JvmUtil.memoryLayout());
+    }
 }
