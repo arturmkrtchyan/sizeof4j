52bd9920612b48e9f98f551b6ed6fa24f39be7a0
Artur Mkrtchyan <mkrtchyan.artur@gmail.com>
2015-04-27 21:40:07 +0200

    Added empty Layout class

---
+++ src/main/java/com/arturmkrtchyan/sizeof4j/ObjectLayout.java
@@ -0,0 +1,12 @@
+package com.arturmkrtchyan.sizeof4j;
+
+
+public class ObjectLayout {
+    // ->Header
+    //  |
+    //   -> Mark word
+    //   -> Klass Pointer 4 byte (on 32)
+    //   -> Flags         4 byte
+    //   -> Object Fields
+
+}
